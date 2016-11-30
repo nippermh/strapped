1a1fa724937d51d3e80e254c5d1e688c2341eedc
nippermh <nippermh@hotmail.com>
2016-11-30 15:08:21 +1100

    bower bootstrap components added and enqueued

--- functions.php
+++ functions.php
@@ -115,6 +115,8 @@ add_action( 'widgets_init', 'strapped_widgets_init' );
  */
 function strapped_scripts() {
 	wp_enqueue_style( 'strapped-style', get_stylesheet_uri() );
+    
+    wp_enqueue_style( 'bootstrap-styles', get_stylesheet_uri() . 'bower_components/dist/css/boostrap.min.css', array(), '3.3.7', 'all' );
 
 	wp_enqueue_script( 'strapped-navigation', get_template_directory_uri() . '/js/navigation.js', array(), '20151215', true );
 
