860b5271f6460c4c7d6176e7b87bc8837bf5e7e4
nippermh <nippermh@hotmail.com>
2016-12-04 15:55:18 +1100

    Style Search form with Bootstrap classes

--- inc/scripts.php
+++ inc/scripts.php
@@ -11,6 +11,10 @@ function strapped_scripts() {
     
     wp_enqueue_style( 'font-awesome', get_template_directory_uri() . 'bower_components/fontawesome/css/font-awesome.min.css', array(), '4.7.0', 'all' );
     
+    wp_enqueue_style( 'google-body', '//fonts.googleapis.com/css?family=PT+Sans:400,400i,700,700i');
+
+	wp_enqueue_style( 'google-heading', '//fonts.googleapis.com/css?family=Raleway:400,300,700');
+    
 	wp_enqueue_script( 'strapped-navigation', get_template_directory_uri() . '/js/navigation.js', array(), '20151215', true );
 
 	wp_enqueue_script( 'strapped-skip-link-focus-fix', get_template_directory_uri() . '/js/skip-link-focus-fix.js', array(), '20151215', true );
