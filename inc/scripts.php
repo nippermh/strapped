ca75a9a1689cf76d1533368e28dc1bcd62454e3a
nippermh <nippermh@hotmail.com>
2016-12-04 15:13:05 +1100

    Add panels to sidebar

--- inc/scripts.php
+++ inc/scripts.php
@@ -9,6 +9,8 @@ function strapped_scripts() {
 
     wp_enqueue_script( 'strapped-js', get_template_directory_uri() . '/js/dist/scripts.min.js', array('jquery'), '1.0.0', true );
     
+    wp_enqueue_style( 'font-awesome', get_template_directory_uri() . 'bower_components/fontawesome/css/font-awesome.min.css', array(), '4.7.0', 'all' );
+    
 	wp_enqueue_script( 'strapped-navigation', get_template_directory_uri() . '/js/navigation.js', array(), '20151215', true );
 
 	wp_enqueue_script( 'strapped-skip-link-focus-fix', get_template_directory_uri() . '/js/skip-link-focus-fix.js', array(), '20151215', true );
